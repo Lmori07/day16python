93db7c1a1669d0ac00c7d723c2a22bf673a4c8cf
Jose Osv. Morillo Villar <joseomv07@gmail.com>
2024-10-19 12:20:47 -0400

    Practicing.....

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 
 darry = Turtle()
 print(darry)
+darry.shape('turtle')
+darry.color('DarkOrange')
+darry.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
