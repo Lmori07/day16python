7701b20655bb9b7e44771e4192bf684d8a418b6c
Jose Osv. Morillo Villar <joseomv07@gmail.com>
2024-10-20 12:10:48 -0400

    Last update.

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from turtle import Turtle, Screen
+from prettytable import PrettyTable
 
-darry = Turtle()
-print(darry)
-darry.shape('turtle')
-darry.color('DarkOrange')
-darry.forward(100)
+# darry = Turtle()
+# print(darry)
+# darry.shape('turtle')
+# darry.color('DarkOrange')
+# darry.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu","Squirtle","Charmander"])
+table.add_column("Type", ["Electric","Water","Fire"])
+print(table)
